a06b8dd56b4d086651e686d6bb320dc51d1fa1c9
MatthewNaugle <mjn00007@mix.wvu.edu>
2024-02-29 17:25:14 -0500

    Modified Card and Navbar/ removed logo

--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,38 @@
-<div class="card-deck">
-  <div class="card">
-  <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Card 01</h5>
-      <p class="card-text">This is the blue card!</p>
+<div class="row  row-cols-1 row-cols-md-3 mb-3 text-center">
+  <div class="col">
+    <div class="card">
+    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">
+              <h3>Card 01</h3>
+        </div>
+        <div class="card-body">
+          <p>This is the blue card!</p>
+        </div>
     </div>
     </div>
   </div>
-  <div class="card">
-  <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Card 02</h5>
-      <p class="card-text">This is the red card!</p>
+
+ <div class="col">
+    <div class="card">
+    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+        <div class="card-header">
+              <h3>Card 02</h3>
+        </div>
+        <div class="card-body">
+          <p>This is the red card!</p>
+        </div>
     </div>
     </div>
   </div>
-  <div class="card">
-  <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Card 03</h5>
-      <p class="card-text">This is the green card!</p>
+  <div class="col">
+    <div class="card">
+    <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+        <div class="card-header">
+              <h3>Card 03</h3>
+        </div>
+        <div class="card-body">
+          <p>This is the green card!</p>
+        </div>
     </div>
     </div>
   </div>
