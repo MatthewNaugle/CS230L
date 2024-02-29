54c37a972e1fbd48155b206ab60db0f47dfe97a4
MatthewNaugle <mjn00007@mix.wvu.edu>
2024-02-29 16:45:07 -0500

    Added Navbar and Cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <h2>Section-01</h2>
       <p>WVU ID: 800384638</p>
       <p>Hi, I am Matthew Naugle</p>
+      <Navbar />
+      <Card />
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,26 @@
+<div class="card-deck">
+  <div class="card">
+  <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Card 01</h5>
+      <p class="card-text">This is the blue card!</p>
+    </div>
+    </div>
+  </div>
+  <div class="card">
+  <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Card 02</h5>
+      <p class="card-text">This is the red card!</p>
+    </div>
+    </div>
+  </div>
+  <div class="card">
+  <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Card 03</h5>
+      <p class="card-text">This is the green card!</p>
+    </div>
+    </div>
+  </div>
+</div>
