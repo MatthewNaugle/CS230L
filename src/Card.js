8ffa49da4425154a191a4bbf6b8e2b7ce425be9c
MatthewNaugle <mjn00007@mix.wvu.edu>
2024-02-29 17:49:29 -0500

    Card and Navbar to JSX

--- src/Card.js
+++ src/Card.js
@@ -1,44 +1,44 @@
 const Card = () => {
     return (
-<div class="row  row-cols-1 row-cols-md-3 mb-3 text-center">
-  <div class="col">
-    <div class="card">
-    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-        <div class="card-header">
-              <h3>Card 01</h3>
-        </div>
-        <div class="card-body">
-          <p>This is the blue card!</p>
-        </div>
-    </div>
-    </div>
-  </div>
+        <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div className="col">
+                <div className="card">
+                    <div className="card text-white bg-primary mb-3" style={{ maxWidth: "18rem" }}>
+                        <div className="card-header">
+                            <h3>Card 01</h3>
+                        </div>
+                        <div className="card-body">
+                            <p>This is the blue card!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
- <div class="col">
-    <div class="card">
-    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-        <div class="card-header">
-              <h3>Card 02</h3>
+            <div className="col">
+                <div className="card">
+                    <div className="card text-white bg-danger mb-3" style={{ maxWidth: "18rem" }}>
+                        <div className="card-header">
+                            <h3>Card 02</h3>
+                        </div>
+                        <div className="card-body">
+                            <p>This is the red card!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <div className="card text-white bg-success mb-3" style={{ maxWidth: "18rem" }}>
+                        <div className="card-header">
+                            <h3>Card 03</h3>
+                        </div>
+                        <div className="card-body">
+                            <p>This is the green card!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-          <p>This is the red card!</p>
-        </div>
-    </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-    <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-        <div class="card-header">
-              <h3>Card 03</h3>
-        </div>
-        <div class="card-body">
-          <p>This is the green card!</p>
-        </div>
-    </div>
-    </div>
-  </div>
-</div>
     )
 }
 
